60d15fcfd86f57cd6dda071de81cedf80c9fd2ef
Tomas Doherty <tomasdoh1@gmail.com>
2019-02-13 20:17:18 +0000

    Refactoring spec with beforeEach

--- spec/unit/AccountSpec.js
+++ spec/unit/AccountSpec.js
@@ -1,23 +1,24 @@
 describe('Account', function () {
 
-const Account = require('../../src/Account')
+  const Account = require('../../src/Account')
 
-let account;
+  let account;
 
-  it('a new account has a default balance of 0', function() {
+  beforeEach(function () {
     account = new Account();
+  })
+
+  it('a new account has a default balance of 0', function () {
     expect(account.getBalance()).toEqual(0)
   });
 
 
-  it('user can deposit money into account', function() {
-    account = new Account();
+  it('user can deposit money into account', function () {
     account.deposit(25)
     expect(account.getBalance()).toEqual(25);
   });
 
-  it('user can withdraw money from account', function() {
-    account = new Account();
+  it('user can withdraw money from account', function () {
     account.deposit(25)
     account.withdraw(20)
     expect(account.getBalance()).toEqual(5);
