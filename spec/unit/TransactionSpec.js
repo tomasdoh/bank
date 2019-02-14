a5c748a8fc6dad4195ed43ee6b26c8ae97fd23bb
Tomas Doherty <tomasdoh1@gmail.com>
2019-02-14 17:14:54 +0000

    Failing test for transaction
    Test might need to be refactored depending on production code

--- spec/unit/TransactionSpec.js
+++ spec/unit/TransactionSpec.js
@@ -1,6 +1,12 @@
 describe('Transaction', () => {
-  const Transaction = require('./src/Transaction')
+  const Transaction = require('../../src/Transaction')
 
   let transaction;
-  
-})
+
+  it('each transaction is dated', () => {
+    const date = new Date();
+    const transaction = new Transaction();
+
+    expect(transaction).toEqual(date)
+  });
+});
